4e8be14547f99275799667b0fd54340b3f6fde8b
Karim Ossama Elmasry <karimelmasry42@icloud.com>
2024-04-22 17:46:22 +0200

    Simplified header css

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
 <header class="main-header">
-    <a href="index.php" class="main-header__logo">TRAVEL</a>
-    <nav>
+    <a href="index.php" class="main-logo">TRAVEL</a>
+    <nav class="navbar">
         <ul>
-            <li class="main-header__nav-item"><a class="main-header__nav-link" href="index.php">Home</a></li>
-            <li class="main-header__nav-item"><a class="main-header__nav-link" href="about.php">About us</a></li>
-            <li class="main-header__nav-item"><a class="main-header__nav-link" href="packages.php">Packages</a></li>
-            <li class="main-header__nav-item"><a class="main-header__nav-link" href="book.php">Book</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About us</a></li>
+            <li><a href="packages.php">Packages</a></li>
+            <li><a href="book.php">Book</a></li>
         </ul>
     </nav>
+    <div id="navbar-menu-button">&#9776;</div>
 </header>
