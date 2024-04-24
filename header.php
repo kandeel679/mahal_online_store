d7e71f2a952e9cb5fe20d6d9d543d08229c59a54
Karim Ossama Elmasry <karimelmasry42@icloud.com>
2024-04-24 21:43:19 +0200

    header done

--- header.php
+++ header.php
@@ -1,12 +1,9 @@
-<header class="main-header">
-    <a href="index.php" class="main-logo">TRAVEL</a>
+<header>
+    <a href="index.php" class="logo">TRAVEL</a>
     <nav class="navbar">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About us</a></li>
-            <li><a href="packages.php">Packages</a></li>
-            <li><a href="book.php">Book</a></li>
-        </ul>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="packages.php">Packages</a>
+        <a href="book.php">Book</a>
     </nav>
-    <div id="navbar-menu-button">&#9776;</div>
 </header>
--- index.php
+++ index.php
@@ -4,25 +4,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <!-- SwiperJS CSS for touch-friendly sliders -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <!-- FontAwesome CSS from CDNJS for icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/style.css">
+    <title>Travel</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <?php
     include("header.php");
     ?>
-    <!-- NOTE: JS files are placed last to make the page appear to load faster -->
-    <!--       and allow JS to edit DOM (html needs to be loaded first) -->
-    <!-- Swiper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <!-- Custom JS  -->
-    <script src="js/script.js"></script>
+    <main>
+        <h1>Home</h1>
+        <p>Welcome to our travel website. We offer a wide range of travel packages to suit your needs.</p>
+        <p>Click on the links above to learn more about us, our packages, and how to book.</p>
+        <p>Enjoy your stay!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nisi eos, doloremque voluptates explicabo ab molestiae nemo eius libero alias esse necessitatibus reprehenderit sequi maiores sed. Fuga iusto facere commodi!</p>
+    </main>
+    <footer>
+        &copy; 2020 Travel
+    </footer>
 </body>
 
 </html>
